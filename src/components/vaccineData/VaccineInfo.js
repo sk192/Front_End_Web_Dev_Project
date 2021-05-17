07a1a125e135f50e301fb954d6057545375c7d05
sk192 <korulkar.shweta7@gmail.com>
2021-05-17 18:55:21 +0530

    added bar chart

--- src/components/vaccineData/VaccineInfo.js
+++ src/components/vaccineData/VaccineInfo.js
@@ -1,55 +1,51 @@
-import axios from 'axios';
-import React, { Component } from 'react';
-import Map from './worldmap'; 
-class VaccineInfo extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            arr : [],
-        
-        }
-    }
+import axios from "axios";
+import React, { Component } from "react";
+import Barchart from "./barchart";
+import "./vaccineInfo.css";
+class VaccineInfo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      arr: [],
+    };
+  }
 
-    componentDidMount = async () => {
-        try{
-            const url = './dataset/vaccine_data_global.json';
-            let responseData = await axios.get(url);
-            console.log(responseData);
-            let a = [];
-        
-            responseData.data.forEach(element => {
-                let new_arr = {}
-                if(element.Province_State === ""){
-                    new_arr['country']= element.Country_Region;
-                    new_arr['People_fully_vaccinated']= element.People_fully_vaccinated;
-                    new_arr['People_partially_vaccinated']= element.People_partially_vaccinated;  
-                    new_arr['Doses_admin'] = element.Doses_admin; 
-                    new_arr['value'] = element.UID;
-                    a.push(new_arr);
-                }
-                
-            });
-            
-            this.setState({
-                arr : a,
-            })
-            console.log(this.state.arr);
+  componentDidMount = async () => {
+    try {
+      const url = "./dataset/vaccine_data_global.json";
+      let responseData = await axios.get(url);
+      console.log(responseData);
+      let a = [];
 
+      responseData.data.forEach((element) => {
+        let new_arr = {};
+        if (element.Province_State === "") {
+          new_arr["country"] = element.Country_Region;
+          new_arr["People_fully_vaccinated"] = element.People_fully_vaccinated;
+          new_arr["People_partially_vaccinated"] =
+            element.People_partially_vaccinated;
+          new_arr["Doses_admin"] = element.Doses_admin;
+          new_arr["value"] = element.UID;
+          a.push(new_arr);
         }
-        catch(err){
-            console.log(err);
-        }
-    
+      });
+
+      this.setState({
+        arr: a,
+      });
+      console.log(this.state.arr);
+    } catch (err) {
+      console.log(err);
     }
-    render(){
-        return(
-            <div>
-                
-                <Map  />
-            </div>
-        )
-     
-    }
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <Barchart />
+      </div>
+    );
+  }
 }
 
 export default VaccineInfo;
