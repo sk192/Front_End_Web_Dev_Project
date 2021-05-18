1a42e3b9d8413f13f7a651e02b91c862a0ad5a29
sk192 <korulkar.shweta7@gmail.com>
2021-05-19 00:15:54 +0530

    imported footer

--- src/App.js
+++ src/App.js
@@ -1,31 +1,29 @@
-import React from 'react';
-import './App.css';
-import './css/cases.css';
-import HeaderNav from './components/navbar'
-import HomePage from './components/HomePage';
-import VaccineInfo from './components/vaccineData/VaccineInfo';
-import CovidInfo from './components/cases/covidInfo';
-import About from './components/about';
+import React from "react";
+import "./App.css";
+import "./css/cases.css";
+import HeaderNav from "./components/navbar.jsx";
+import HomePage from "./components/HomePage.jsx";
+import VaccineInfo from "./components/vaccineData/VaccineInfo.jsx";
+import CovidInfo from "./components/cases/covidInfo";
+import About from "./components/about";
+import Footer from "./components/footer.jsx";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
-
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
-
-  <div className="App">
+    <div className="App">
       <div className="content-wrap">
-  
-          <Router>
-              <HeaderNav />
-              <Switch>
-                  <Route exact path ="/" component= {HomePage} />
-                  <Route exact path ="/vaccination" component= {VaccineInfo} />
-                  <Route exact path ="/covidInfo" component= {CovidInfo} />
-                  <Route exact path ="/about" component= {About} />
-              </Switch>
-          </Router>
-
+        <Router>
+          <HeaderNav />
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/vaccination" component={VaccineInfo} />
+            <Route exact path="/covidInfo" component={CovidInfo} />
+            <Route exact path="/about" component={About} />
+          </Switch>
+        </Router>
       </div>
+      <Footer />
     </div>
   );
 }
