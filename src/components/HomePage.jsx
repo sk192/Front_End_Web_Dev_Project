73f3b6cb757b0a35ad544a24e1d225ecc9249367
sk192 <korulkar.shweta7@gmail.com>
2021-05-19 00:27:00 +0530

    changed class name

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './homePage.css';
+
+const HomePage = () => {
+    return(
+        <div className='homepage'>
+            <h1>HomePage</h1>
+        </div>
+    )
+}
+
+export default HomePage;
