dc130d6db3b695803abe9544030b87b4d56a8b64
Sree-Vandana <sreevandana.sv@gmail.com>
2021-05-23 14:15:25 -0700

    adding about page cards

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,7 +1,36 @@
+// import pic from "../images/pic1.jpg";
+import pic from "../components/images/pic1.jpg";
+
 export default function About() {
   return (
     <div className="about-body">
-      <h3>About Page</h3>
+      <div className="aboutCard about1">
+        <img src={pic} alt="Avatar" className="aboutImg" />
+        <div className="aboutContainer">
+          <h4 className="about-name">
+            <b>Shweta Korulkar</b>
+          </h4>
+          <p className="about-task">Task: VaccinationInfo Page and Home page</p>
+          <a href="https://github.com/sk192" className="about-gitrepo-link">
+            GitHub
+          </a>
+        </div>
+      </div>
+      <div className="aboutCard about2">
+        <img src={pic} alt="Avatar" className="aboutImg" />
+        <div className="aboutContainer">
+          <h4 className="about-name">
+            <b>Sree Vandana</b>
+          </h4>
+          <p className="about-task">Task: Cases Status Page and About Page</p>
+          <a
+            href="https://github.com/Sree-Vandana"
+            className="about-gitrepo-link"
+          >
+            GitHub
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
