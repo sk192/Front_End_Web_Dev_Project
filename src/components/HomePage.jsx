d4d4d4ff4f7297223634b9c9678e46ef2a329a5d
sk192 <korulkar.shweta7@gmail.com>
2021-05-26 18:32:40 +0530

    added text

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -4,7 +4,9 @@ import './homePage.css';
 const HomePage = () => {
     return(
         <div className='homepage'>
-            <h1>HomePage</h1>
+            <h1>NAVIGATE TO PAGES</h1>
+            <h1>AND</h1>
+            <h1> GET COVID-19 STATISTICS</h1>
         </div>
     )
 }
