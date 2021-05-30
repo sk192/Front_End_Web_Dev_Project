41877c0f57c0646e75c9de31a126edcae7962b18
Sree-Vandana <sreevandana.sv@gmail.com>
2021-05-30 10:25:47 -0700

    adding navbar and nav css, and final cases css

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,40 +1,40 @@
+import React from "react";
 import "./navbar.css";
+import { Nav, Navbar, NavDropdown } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function HeaderNav() {
   return (
     <>
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <button class="navbar-brand bg-dark">COVID-19 DASHBOARD</button>
-        <div class="navbar-collapse collapse" id="collapsingNavbarMd">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link" href="/">
-                <h4 class="links">Home</h4>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/vaccination">
-                <h4 class="links">Vaccination_Info</h4>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/vaccineCompany">
-                <h4 class="links">VaccineManufacturerInfo</h4>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/covidInfo">
-                <h4 class="links">covidInfo</h4>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/about">
-                <h4 class="links">About</h4>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
+      <Navbar
+          collapseOnSelect
+          expand="md"
+          variant="dark"
+          className="navBarColor"
+      >
+        <Navbar.Brand href="/">Covid-19 Dashboard</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <NavDropdown title="Vaccination" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="/vaccination">
+                Vaccination Status
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="/vaccineCompany">
+                Vaccine Manufacture
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Nav.Link href="/covidInfo">Covid Info</Nav.Link>
+            <Nav.Link eventKey={2} href="/about">
+              About
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     </>
   );
 }
